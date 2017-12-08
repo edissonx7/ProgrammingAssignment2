038fdce35c9f442da337db2209f6955081d27dde
edisson <edisson.lee@163.com>
2017-12-09 00:39:23 +0800

    init

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,48 @@
-## Put comments here that give an overall description of what your
-## functions do
+## These 2 functions help to create a list for storing a matrix and caching its inserversion.
 
-## Write a short comment describing this function
+# Examples
+# > cm <- makeCacheMatrix( matrix(1:4,2,2) )
+# > cacheSolve(cm)
+# [,1] [,2]
+# [1,]   -2  1.5
+# [2,]    1 -0.5
+# > cacheSolve(cm) %*% cm$get()
+# getting cached data
+# [,1] [,2]
+# [1,]    1    0
+# [2,]    0    1
+
+## Creates a special "Matrix", which is a list containing 4 functions 
+## that include set, get, setinversion and getinversion
+## x must be a square matrix
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  i <- NULL
+  set <- function(y) {
+    x <<- y
+    i <<- NULL
+  }
+  get <- function() x
+  setinversion <- function(inversion) i <<- inversion
+  geinversion <- function() i
+  list(set = set, get = get,
+       setinversion = setinversion,
+       geinversion = geinversion)    
 }
 
 
-## Write a short comment describing this function
+## Return a matrix that is the inverse of 'x'
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+
+  i <- x$geinversion()
+  if(!is.null(i)) {
+    message("getting cached data")
+    return(i)
+  }
+  data <- x$get()
+  i <- solve(data, ...)
+  x$setinversion(i)
+  i  
+    
 }
